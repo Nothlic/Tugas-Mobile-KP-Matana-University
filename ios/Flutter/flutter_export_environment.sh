4fff4a2fe40bdb6ac0320cc66c076be8f2241e9a
Anton Kurniawan <45499589+Nothlic@users.noreply.github.com>
2019-11-22 07:09:56 +0700

    Initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nothlic/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nothlic/AndroidStudioProjects/material-components-flutter-codelabs-101-starter/mdc_100_series"
+export "FLUTTER_TARGET=/Users/nothlic/AndroidStudioProjects/material-components-flutter-codelabs-101-starter/mdc_100_series/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/nothlic/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
